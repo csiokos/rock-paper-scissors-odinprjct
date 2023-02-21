c55b5555742cb09cb0c7d554a5fafc0f41be32d5
csiokos <123376412+csiokos@users.noreply.github.com>
2023-02-21 20:12:59 +0200

    first stages of rock paper scissors with ui. includes css

--- javascript.js
+++ javascript.js
@@ -1,71 +1,110 @@
-function getComputerChoice(){
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex]
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 5;
+
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+    
     if (playerSelection === computerSelection) {
-      return "Draw!";
+        return "Draw!";
     } else if (playerSelection === "rock") {
-      if (computerSelection === "scissors") {
-        return "winner";
-      } else {
-        return "loser";
-      }
+    if (computerSelection === "scissors") {
+      playerScore++;
+        return "You Win! Rock beats Scissors";
+    } else {
+      computerScore++;
+      return "You Lose! Paper beats Rock";
+    }
     } else if (playerSelection === "paper") {
       if (computerSelection === "rock") {
-        return "winner";
+        playerScore++;
+        return "You Win! Paper beats Rock";
       } else {
-        return "loser";
+        computerScore++;
+          return "You Lose! Scissors beats Paper";
       }
     } else if (playerSelection === "scissors") {
       if (computerSelection === "paper") {
-        return "winner";
+        playerScore++;
+        return "You Win! Scissors beats Paper";
       } else {
-        return "loser";
-      }
+        computerScore++;
+        return "You Lose! Rock beats Scissors";
+          }
     } else {
-      return "Invalid input. Please enter Rock, Paper, or Scissors.";
+          return "Invalid input. Please enter Rock, Paper, or Scissors.";
     }
-  }
+}
 
-  function game(){
-    let scorePlayer = 0;
-    let scoreComp = 0;
-    for (let i=0; i<5; i++ ){
-        let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        let computerSelection = (getComputerChoice()).toLowerCase();
-        console.log(playerSelection, computerSelection)
-        let result = playRound(playerSelection,computerSelection)
-        if(result === 'winner'){
-            scorePlayer= scorePlayer+1
+function updateScore() {
+  const scoreDiv = document.querySelector('#score');
+  scoreDiv.innerHTML = `Player: ${playerScore} - Computer: ${computerScore}`;
 
-        } else if(result==='loser'){
-            scoreComp = scoreComp +1;
-            
-        }else{
-            i=i-1;
+    if (playerScore === winningScore || computerScore === winningScore) {
+        const winnerDiv = document.querySelector('#winner');
+        const playAgainButton = document.querySelector('#play-again-button');
+        if (playerScore > computerScore) {
+          winnerDiv.innerHTML = "You Win!";
+        } else {
+          winnerDiv.innerHTML = "You Lose!";
         }
-        console.log("Player:", scorePlayer);
-        console.log("Comp:", scoreComp);
+        playAgainButton.style.display = 'block';
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
     }
-    let final;
-    if(scoreComp>scorePlayer){
-        final = "You lose";
-        return final;
-    } else if(scoreComp<scorePlayer) {
-        final = "You win"; 
-        return final;
+}
 
-    } else {
-        final = "tie";
-        return final;
-    }
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  const resultDiv = document.querySelector('#result');
+  const winnerDiv = document.querySelector('#winner');
+  const playAgainButton = document.querySelector('#play-again-button');
+  resultDiv.innerHTML = '';
+  winnerDiv.innerHTML = '';
+  playAgainButton.style.display = 'none';
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
+}
 
-  }
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+const playAgainButton = document.querySelector('#play-again-button');
 
+rockButton.addEventListener('click', () => {
+  const result = playRound('rock', getComputerChoice());
+  updateScore();
+  const resultDiv = document.querySelector('#result');
+  resultDiv.innerHTML = result;
+});
 
-  alert(game());
-  
-  
+paperButton.addEventListener('click', () => {
+  const result = playRound('paper', getComputerChoice());
+  updateScore();
+  const resultDiv = document.querySelector('#result');
+  resultDiv.innerHTML = result;
+});
+
+scissorsButton.addEventListener('click', () => {
+  const result = playRound('scissors', getComputerChoice());
+  updateScore();
+  const resultDiv = document.querySelector('#result');
+  resultDiv.innerHTML = result;
+});
+
+playAgainButton.addEventListener('click', () => {
+  resetGame();
+});
+
+//den exo xrisimopoiisei tin teleutaia protasi tou chatgpt
